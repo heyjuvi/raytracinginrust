084e72f676f253e740a1518a77010b2fea08c648
Daniel Busch <daniel.busch@kernelconcepts.de>
2021-05-10 20:52:48 +0200

    Make more progress

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -13,7 +13,7 @@ use vec::{Point3, Color};
 use ray::Ray;
 use hit::{Hit, World};
 use sphere::Sphere;
-use material::{Lambertian, Metal};
+use material::{Lambertian, Metal, Dielectric};
 use camera::Camera;
 
 fn ray_color(r: &Ray, world: &World, depth: u64) -> Color {
@@ -38,7 +38,7 @@ fn ray_color(r: &Ray, world: &World, depth: u64) -> Color {
 fn main() -> () {
     // Image
     const ASPECT_RATIO: f64 = 16.0 / 9.0;
-    const IMAGE_WIDTH: u64 = 256;
+    const IMAGE_WIDTH: u64 = 512;
     const IMAGE_HEIGHT: u64 = ((IMAGE_WIDTH as f64) / ASPECT_RATIO) as u64;
     const SAMPLES_PER_PIXEL: u64 = 300;
     const MAX_DEPTH: u64 = 50;
@@ -47,18 +47,21 @@ fn main() -> () {
     let mut world = World::new();
 
     let mat_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
-    let mat_center = Rc::new(Lambertian::new(Color::new(0.7, 0.3, 0.3)));
-    let mat_left = Rc::new(Metal::new(Color::new(0.8, 0.8, 0.8)));
-    let mat_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2)));
+    let mat_center = Rc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
+    let mat_left = Rc::new(Dielectric::new(1.5));
+    let mat_left_inner = Rc::new(Dielectric::new(1.5));
+    let mat_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
 
     let sphere_ground = Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, mat_ground);
     let sphere_center = Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5, mat_center);
     let sphere_left = Sphere::new(Point3::new(-1.0, 0.0, -1.0), 0.5, mat_left);
+    let sphere_left_inner = Sphere::new(Point3::new(-1.0, 0.0, -1.0), -0.4, mat_left_inner);
     let sphere_right = Sphere::new(Point3::new(1.0, 0.0, -1.0), 0.5, mat_right);
 
     world.push(Box::new(sphere_ground));
     world.push(Box::new(sphere_center));
     world.push(Box::new(sphere_left));
+    world.push(Box::new(sphere_left_inner));
     world.push(Box::new(sphere_right));
 
     // Camera
